2d10d374f8b29cb25b1b9db3ae528eb2fd1be590
Boryslav <kuchkovborislav000@gmail.com>
2021-05-07 18:44:40 +0300

    Refactor product_one.php

--- views/product_one.php
+++ views/product_one.php
@@ -12,7 +12,7 @@
     <div class="card shadow-sm">
       <svg class="bd-placeholder-img card-img-top" width="100%" height="225" img="" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
       <div class="card-body">
-        <p><?php  ?></p>
+        <p></p>
         <p class="card-text"></p>
         <div class="d-flex justify-content-between align-items-center">
           <div class="btn-group">
