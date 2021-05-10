3db2a13aca114578e07ff59298b3df0592b5b847
Boryslav <kuchkovborislav000@gmail.com>
2021-05-10 17:55:33 +0300

    Create Autoloader

--- index.php
+++ index.php
@@ -3,18 +3,7 @@
 use Framework\Application;
 use Framework\Router\Router;
 
-include_once __DIR__ . '/../autoloader.php';
+include_once __DIR__ . '/autoloader.php';
 $autoloader = new Autoloader();
 spl_autoload_register([$autoloader, 'load']);
 
-var_dump($_SERVER);
-
-$app = new Application();
-
-$app->router->get('/', 'home');
-$app->router->get('/public/index.php/contact', function(){
-    return 'Contact';
-});
-
-$app->run();
-?>
