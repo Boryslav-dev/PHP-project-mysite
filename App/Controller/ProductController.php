235b52efec2761f522ce255db639e3d499cf7c5c
Boryslav <kuchkovborislav000@gmail.com>
2021-05-13 16:36:43 +0300

    Modify some methods

---
+++ App/Controller/ProductController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use App\Model\Product;
+use Framework\Core\Controller;
+
+class ProductController extends Controller
+{
+
+    protected $productController;
+
+    public function getProductList()
+    {
+        $this->productController = new Product();
+        $params = $this->productController->getAllProduct();
+        return $this->view->render('product_list.php', $params, 'site.php');
+    }
+
+    public function getProductById($id)
+    {
+        $this->productController = new Product();
+        $params = $this->productController->getOneProduct($id);
+        return$this->view->render('product_one.php', $params, 'site.php');
+    }
+
+}
