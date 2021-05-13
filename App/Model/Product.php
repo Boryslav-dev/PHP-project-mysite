ef67209b83db205bcf595b75561deb71a1d9e30c
Boryslav <kuchkovborislav000@gmail.com>
2021-05-13 16:34:47 +0300

    Create custom model

---
+++ App/Model/Product.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Model;
+
+use Framework\Core\Model;
+
+class Product extends Model
+{
+    protected $id;
+
+    protected $name;
+
+    protected $category;
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function getCategory(): string
+    {
+        return $this->category;
+    }
+
+    public function getAllProduct(): array
+    {
+        return $this->file;
+    }
+
+    public function getOneProduct($id): array
+    {
+
+        $products = $this->file;
+
+        if ($id !== $products[$id]['id']) {
+            echo ($id);
+        }
+        else {
+            $data = ($products[$id]);
+        }
+        return $data;
+    }
+
+    protected static function getTableName(): string
+    {
+        return "Product";
+    }
+
+}
