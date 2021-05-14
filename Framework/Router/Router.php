5fe70d7baa29dd644b4d256b20c98309f24208da
Boryslav <kuchkovborislav000@gmail.com>
2021-05-14 23:33:02 +0300

    Add echo to product

--- Framework/Router/Router.php
+++ Framework/Router/Router.php
@@ -28,12 +28,15 @@ class Router
     {
         $method = $this->request->getMethod();
         $url = $this->request->getUrl();
+        preg_match('/^\/(.+)\/(.+)$/', $url, $matches);
+        print_r($matches);
         $callback = $this->routeMap[$method][$url] ?? false;
+        print_r($callback);
         if (is_array($callback)) {
-            $controller = new $callback[0];
+            $controller = new $callback[0]();
             $callback[0] = $controller;
             $action = $callback[1];
         }
-        return call_user_func($callback, $this->request);
+        return call_user_func($callback, $matches[2]);
     }
 }
