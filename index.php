714530950cdd23846c89ecb089a7f2c2baf72e41
Boryslav <kuchkovborislav000@gmail.com>
2021-05-15 00:29:04 +0300

    Add urls

--- index.php
+++ index.php
@@ -5,16 +5,15 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 
 use Framework\Application;
-use Framework\Router\Router;
 
 include_once 'vendor/autoload.php';
 
 $app = new Application();
 
 $app->router->get('/', [\App\Controller\ProductController::class, 'getProductList']);
-$app->router->get('/product/{id}', [\App\Controller\ProductController::class, 'getProductById']);
-$app->router->get('/login', [\App\Controller\LoginController::class, 'authorization']);
-$app->router->get('/logout', [\App\Controller\LoginController::class, 'logout']);
-$app->router->post('/login', [\App\Controller\LoginController::class, 'login']);
+$app->router->get('/product/(\d+)', [\App\Controller\ProductController::class, 'getProductById']);
+$app->router->get('/logout/', [\App\Controller\LoginController::class, 'logout']);
+$app->router->post('/login/send/', [\App\Controller\LoginController::class, 'login']);
+$app->router->get('/login/', [\App\Controller\LoginController::class, 'authorization']);
 
 $app->run();
