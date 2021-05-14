5f56977f52316648e0c244cf9c316df69d79c363
Boryslav <kuchkovborislav000@gmail.com>
2021-05-15 00:27:35 +0300

    Change urls

--- App/View/Templates/Authorization/login.php
+++ App/View/Templates/Authorization/login.php
@@ -1,6 +1,6 @@
 
 <main class="main text-center align-item-center d-flex justify-content-center form-signin mt-5">
-  <form action="/login" method="post">
+  <form action="/login/send/" method="post">
     <img class="mb-4" src="" alt="" width="72" height="57">
     <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
     <label for="inputLogin" class="visually-hidden">Login</label>
@@ -8,6 +8,5 @@
     <label for="inputPassword" class="visually-hidden">Password</label>
     <input type="password" name="password" id="inputPassword" class="form-control" placeholder="Password" required>
     <button class="w-100 btn btn-lg btn-primary mt-5" name="submit" type="submit">Sign in</button>
-      <?php echo $message; ?>
   </form>
 </main>
