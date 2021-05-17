38c6765bbdc90965f248493b6c5e3812da6e3fe4
Boryslav <kuchkovborislav000@gmail.com>
2021-05-17 15:21:44 +0300

    Change views layouts

--- App/View/Layouts/header.php
+++ App/View/Layouts/header.php
@@ -9,11 +9,11 @@
                 <li><a href="/" class="nav-link px-2 text-white">Product List</a></li>
                 <li><a href="#" class="nav-link px-2 text-white">Bucket</a></li>
             </ul>
-            <?php $sessionName = session_name();
-                if (isset($_COOKIE[$sessionName]) || isset($_REQUEST[$sessionName]))
-                session_start();
-             ?>
-            <?php if(!isset($_SESSION['login'])): ?>
+            <?php $session = new Framework\Session\Session();
+            if ($session->cookieExists()) {
+                $session->start();
+            }
+            if (!isset($_SESSION['login'])): ?>
             <div class="text-end">
                 <a href="/login/" class="btn btn-outline-light me-2">Login</a>
                 <button type="button" class="btn btn-warning">Sign-up</button>
