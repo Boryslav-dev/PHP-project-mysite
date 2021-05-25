f4b3ec2309ffd27365c1b124bbe678956b312001
Boryslav <kuchkovborislav000@gmail.com>
2021-05-25 15:45:03 +0300

    Add use DataBase

--- Framework/Application.php
+++ Framework/Application.php
@@ -2,18 +2,24 @@
 
 namespace Framework;
 
+use Framework\Database\Database;
 use Framework\Request\Request;
 use Framework\Router\Router;
 
 class Application
 {
+    public static Application $app;
     public Router $router;
     public Request $request;
+    public Database $db;
+
 
     public function __construct()
     {
+        self::$app = $this;
         $this->request = new Request();
         $this->router = new Router($this->request);
+        $this->db = new Database();
     }
 
     public function run()
