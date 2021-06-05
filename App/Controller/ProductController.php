a8057f8f27ab274aeb0f1ba478bf61a9d9bfc415
Boryslav <kuchkovborislav000@gmail.com>
2021-06-05 21:03:14 +0300

    Added API method and api rout

--- App/Controller/ProductController.php
+++ App/Controller/ProductController.php
@@ -4,10 +4,23 @@ namespace App\Controller;
 
 use App\Model\Product;
 use Framework\Core\Controller;
+use Framework\Core\Model;
+use Framework\Core\View;
 
 class ProductController extends Controller
 {
     protected $product;
+
+    public function __construct()
+    {
+        $this->view = new View();
+        $this->model = new Model();
+    }
+
+    public function index()
+    {
+        return $this->view->render('product_list.php', null, 'site.php');
+    }
 
     public function getProductList()
     {
@@ -17,9 +30,18 @@ class ProductController extends Controller
         return $this->view->render('product_list.php', $params, 'site.php');
     }
 
+    public function getProductListAPI()
+    {
+        $this->product = new Product();
+
+        $params = $this->product->getAllProduct();
+        return json_encode($params);
+    }
+
     public function getProductById($id)
     {
         $this->product = new Product();
+
         $params = $this->product->getOneProduct($id);
         return$this->view->render('product_one.php', $params, 'site.php');
     }
--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
+//error_reporting(E_ALL);
+//ini_set('display_errors', 1);
+//ini_set('display_startup_errors', 1);
 
 use Framework\Application;
 
@@ -10,10 +10,13 @@ include_once 'vendor/autoload.php';
 
 $app = new Application();
 
-$app->router->get('/', [\App\Controller\ProductController::class, 'getProductList']);
+//$app->router->get('/', [\App\Controller\ProductController::class, 'getProductList']);
 $app->router->get('/product/(\d+)', [\App\Controller\ProductController::class, 'getProductById']);
 $app->router->get('/logout/', [\App\Controller\LoginController::class, 'logout']);
 $app->router->post('/login/send/', [\App\Controller\LoginController::class, 'login']);
 $app->router->get('/login/', [\App\Controller\LoginController::class, 'register']);
+
+$app->router->get('/', [\App\Controller\ProductController::class, 'index']);
+$app->router->get('/api/', [\App\Controller\ProductController::class, 'getProductListAPI']);
 
 $app->run();
