fa9a50c2d14c42d098b6df9dcc1019b8a1ed37e5
Boryslav <kuchkovborislav000@gmail.com>
2021-06-06 18:56:31 +0300

    Create authorization methods

--- App/Controller/LoginController.php
+++ App/Controller/LoginController.php
@@ -6,11 +6,23 @@ use App\Auth\Auth;
 use App\Model\User;
 use Framework\Application;
 use Framework\Core\Controller;
+use Framework\Core\Model;
+use Framework\Core\View;
 use Framework\Request\Request;
 
 class LoginController extends Controller
 {
     protected Auth $auth;
+
+    protected View $view;
+
+    protected Model $model;
+
+    public function __construct()
+    {
+        $this->view = new View();
+        $this->model = new Model();
+    }
 
     public function register()
     {
@@ -18,6 +30,21 @@ class LoginController extends Controller
     }
 
     public function login()
+    {
+        return $this->view->render('Authorization/login.php', null, 'site.php');
+    }
+
+    public function authorization()
+    {
+        $auth = new Auth();
+        if ($auth->authorization() == true) {
+            return header('Location:/');
+        } else {
+            $this->register();
+        }
+    }
+
+    public function auth()
     {
         $auth = new Auth();
         $auth->isAuth();
