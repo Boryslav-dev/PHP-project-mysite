7b11d6c992b0ec96f1c93c9ade716967b0b8f6dd
Boryslav <kuchkovborislav000@gmail.com>
2021-06-06 18:58:28 +0300

    Add authorization

--- App/View/Templates/Authorization/login.php
+++ App/View/Templates/Authorization/login.php
@@ -4,7 +4,7 @@
     <img class="mb-4" src="" alt="" width="72" height="57">
     <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
     <label for="inputLogin" class="visually-hidden">Login</label>
-    <input type="text" name="login" id="inputLogin" class="form-control" placeholder="Login" required autofocus>
+    <input type="text" name="login" id="inputLogin" class="form-control mb-2" placeholder="Login" required autofocus>
     <label for="inputPassword" class="visually-hidden">Password</label>
     <input type="password" name="password" id="inputPassword" class="form-control" placeholder="Password" required>
     <button class="w-100 btn btn-lg btn-primary mt-5" name="submit" type="submit">Sign in</button>
