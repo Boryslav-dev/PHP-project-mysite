8b4114b290d871a4e4ad9b2b130c85b253f3abc6
Boryslav <kuchkovborislav000@gmail.com>
2021-06-06 18:00:00 +0300

    Add constructor to class

--- App/Controller/ProductController.php
+++ App/Controller/ProductController.php
@@ -4,10 +4,22 @@ namespace App\Controller;
 
 use App\Model\Product;
 use Framework\Core\Controller;
+use Framework\Core\Model;
+use Framework\Core\View;
 
 class ProductController extends Controller
 {
     protected $product;
+
+    protected View $view;
+
+    protected Model $model;
+
+    public function __construct()
+    {
+        $this->view = new View();
+        $this->model = new Model();
+    }
 
     public function getProductList()
     {
