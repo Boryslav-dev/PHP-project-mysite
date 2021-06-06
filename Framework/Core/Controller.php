498bb463bb3affe5958f65cbfb36a83b9d546db1
Boryslav <kuchkovborislav000@gmail.com>
2021-06-06 18:59:16 +0300

    Delete construct

--- Framework/Core/Controller.php
+++ Framework/Core/Controller.php
@@ -4,12 +4,4 @@ namespace Framework\Core;
 
 class Controller
 {
-    public View $view;
-    public Model $model;
-
-    public function __construct()
-    {
-        $this->view = new View();
-        $this->model = new Model();
-    }
 }
