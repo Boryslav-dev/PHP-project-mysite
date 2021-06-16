44ead1e01802b66ebe4e3fb2f345e1ff7833cc7c
Boryslav <kuchkovborislav000@gmail.com>
2021-06-16 13:32:10 +0300

    Commit error

--- index.php
+++ index.php
@@ -1,21 +1,29 @@
 <?php
 
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
+//error_reporting(E_ALL);
+//ini_set('display_errors', 1);
+//ini_set('display_startup_errors', 1);
 
+use App\Controller\LoginController;
+use App\Controller\ProductController;
 use Framework\Application;
 
 include_once 'vendor/autoload.php';
 
 $app = new Application();
 
-$app->router->get('/', [\App\Controller\ProductController::class, 'getProductList']);
-$app->router->get('/product/(\d+)', [\App\Controller\ProductController::class, 'getProductById']);
-$app->router->get('/logout/', [\App\Controller\LoginController::class, 'logout']);
-$app->router->post('/login/send/', [\App\Controller\LoginController::class, 'auth']);
-$app->router->get('/login/', [\App\Controller\LoginController::class, 'login']);
-$app->router->post('/register/send/', [\App\Controller\LoginController::class, 'authorization']);
-$app->router->get('/register/', [\App\Controller\LoginController::class, 'register']);
+//$app->router->get('/', [ProductController::class, 'getProductList']);
+$app->router->get('/product/(\d+)/', [ProductController::class, 'getProductById']);
+$app->router->get('/logout/', [LoginController::class, 'logout']);
+$app->router->post('/login/send/', [LoginController::class, 'auth']);
+$app->router->get('/login/', [LoginController::class, 'login']);
+$app->router->post('/register/send/', [LoginController::class, 'authorization']);
+$app->router->get('/register/', [LoginController::class, 'register']);
+
+$app->router->get('/', [ProductController::class, 'index']);
+//$app->router->get('/getProductListAPI', [ProductController::class, 'getProductListAPI']);
+$app->router->get('/product/', [ProductController::class, 'product']);
+//$app->router->get('/product/(\d+)/', [ProductController::class, 'getProductByIdAPI']);
+$app->router->get('/getProductListAPI/(\d+)/(\w+)/', [ProductController::class, 'getProductListAPI']);
 
 $app->run();
