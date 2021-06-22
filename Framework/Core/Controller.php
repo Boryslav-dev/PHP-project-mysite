ecde9abaddc7c4e9d34c604657a1587054bbb28a
Boryslav <kuchkovborislav000@gmail.com>
2021-06-22 20:12:04 +0300

    Add constructor and Model, View object class

--- Framework/Core/Controller.php
+++ Framework/Core/Controller.php
@@ -4,4 +4,13 @@ namespace Framework\Core;
 
 class Controller
 {
+    protected View $view;
+
+    protected Model $model;
+
+    public function __construct()
+    {
+        $this->view = new View();
+        $this->model = new Model();
+    }
 }
